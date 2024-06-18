16ae5cbd02ceac03354a754ee0ea355022e1dd61
Chakrit Assbilly <chakrit.a@betimes.biz>
2024-06-18 19:43:14 +0700

    feat(carList): create carList component

---
+++ client/src/components/carList.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@mui/material";
+
+const CarList = ({ cars, onEdit, onDelete }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>License Plate</TableCell>
+            <TableCell>Brand</TableCell>
+            <TableCell>Model</TableCell>
+            <TableCell>Notes</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cars.map((car) => (
+            <TableRow key={car.id}>
+              <TableCell>{car.licensePlate}</TableCell>
+              <TableCell>{car.brand}</TableCell>
+              <TableCell>{car.model}</TableCell>
+              <TableCell>{car.notes}</TableCell>
+              <TableCell>
+                <Button
+                  onClick={() => onEdit(car)}
+                  variant="contained"
+                  color="primary"
+                  sx={{ marginRight: 1 }}
+                >
+                  Edit
+                </Button>
+                <Button
+                  onClick={() => onDelete(car.id)}
+                  variant="contained"
+                  color="secondary"
+                >
+                  Delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default CarList;
